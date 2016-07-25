872f448d08d31806d821fd2b499d5d4c1bd573db
Nanda <fernandaalvesmonteiro7@gmail.com>
2016-07-25 20:50:42 -0300

    adicionando pontos

--- target/sosmulher-1.0-SNAPSHOT/resources/js/script.js
+++ target/sosmulher-1.0-SNAPSHOT/resources/js/script.js
@@ -1,40 +1,46 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -6.8873251, lng: -38.5740716},
-    zoom: 15
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
+    var center = {lat: -6.8873251, lng: -38.5740716};
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 15
+    });
+    //var infoWindow = new google.maps.InfoWindow({map: map});
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
 
 //      infoWindow.setPosition(pos);
 //      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-  google.maps.event.addListener(map, 'click', function(event) {
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+    google.maps.event.addListener(map, 'click', function (event) {
         var coordenada = event.latLng;
         document.getElementById('coordenada').value = coordenada;
     });
-    
+
+    new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Hello World!'
+    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
 
 }
